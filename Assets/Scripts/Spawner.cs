7be842e4be68fed55a507c4698650c6e00196dd1
Danopie <ledanofficial@gmail.com>
2016-12-02 19:36:51 +0700

    First commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] blocks;
+	// Use this for initialization
+	void Start () {
+        spawnNext();
+	}
+	
+    public void spawnNext()
+    {
+        int i = Random.Range(0, blocks.Length);
+        Instantiate(blocks[i], transform.position, Quaternion.identity);
+    }
+}
